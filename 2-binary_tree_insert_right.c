5323cb8dbcbfce4878fde86469f67054cdc1d784
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-02-28 03:49:06 -0800

    binary tree

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,39 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right -  a function that inserts a node as the
+ * right-child of another node
+ *
+ * @parent: a pointer to the node to insert the right-child in
+ * @value: is the value to store in the new node
+ * Return: Your function must return a pointer to the created node,
+ * or NULL on failure or if parent is NULL
+ * If parent already has a right-child, the new node must take its place,
+ * and the old right-child must be set as the right-child of the new node.
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode;
+
+	
+	if (parent == NULL)
+		return (NULL);
+
+	newnode = malloc(sizeof(binary_tree_t));
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->n = value;
+	newnode->parent = parent;
+	newnode->left = NULL;
+	newnode->right = NULL;
+	if (parent->right == NULL)
+		parent->right = newnode;
+	else
+	{
+		newnode->n = parent->right->n;
+		parent->right->n = value;
+		parent->right->right = newnode;
+	}
+	return (parent);
+}
