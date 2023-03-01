6196a67855ca512069206572efa549083ebb4a73
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-01 06:43:10 -0800

    binary tree

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,6 +7,20 @@
  * If tree is NULL, the function must return 0. A NULL pointer is not a node
  * Return: size
  */
-size_t binary_tree_nodes(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	size_t count = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree)
+	{
+		count += binary_tree_nodes(tree->left);
+		count += binary_tree_nodes(tree->right);
+		if (tree->left || tree->right)
+			count += 1;
+	}
+
+	return (count);
 }
