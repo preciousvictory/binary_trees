702250fb01b50ba4ccfa21f7ded10dfbb2f6fdd4
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-01 06:02:21 -0800

    binary tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -4,6 +4,7 @@
  * binary_tree_leaves - a function that counts the leaves in a binary tree
  * @tree:  is a pointer to the root node of the treeto count thenumber ofleaves
  * If tree is NULL, the function must return 0, A NULL pointer is not a leaf
+ * Return: number of child
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
@@ -17,5 +18,5 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree->right != NULL)
 		count++;
 
-	return (count);	
+	return (count);
 }
