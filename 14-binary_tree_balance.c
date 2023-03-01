2786e015dbffb1c6163ae720a6c298c2b7826ffb
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-01 07:31:20 -0800

    binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,28 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - function that calculates the height of tree
+ * @tree: is a pointer to the root node of the tree to check
+ * Return: the height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l = 0, r = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left)
+		l = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		r = 1 + binary_tree_height(tree->right);
+
+	if (l > r)
+		return (l);
+	return (r);
+}
+
+/**
  * binary_tree_balance - a function that measures the balance factor of a binary tree
  * @tree: is a pointer to the root node of the tree to measure the balance factor
  * Return: number
@@ -13,13 +35,12 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	
-	left += binary_tree_balance(tree->left);
-	right += binary_tree_balance(tree->right);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 	if (tree->left)
 		left += 1;
 	if (tree->right)
-		right += 1;	
+		right += 1;
 
 	return (left - right);
 }
---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - a function that checks if a binary tree is full
+ * @tree:  is a pointer to the root node of the tree to check
+ * Return: number
+ * If tree is NULL, your function must return 0
+ */
+int binary_tree_is_full(const binary_tree_t *tree);
+{
+	if (tree == NULL)
+		return (0);
+	s
+}
