e9d74f134a3f10bca46b122c60b20f5a91fadb1f
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-01 05:26:23 -0800

    binnary tree

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,14 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - a function that inserts a node
- * 
- *@parent:  is a pointer to the node to insert the left-child in
- *@value: is the value to store in the new node
- *
- * Return: function must return a pointer to the created node,
- * or NULL on failure or if parent is NULL
- */
+* binary_tree_insert_left -  creates a binary tree node
+*@parent: parent of root
+*@value: value of the tree
+* Return: Always 0 (Success)
+*/
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode = NULL;
@@ -17,6 +14,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 
 	newnode = malloc(sizeof(binary_tree_t));
+
 	if (newnode == NULL)
 		return (NULL);
 
