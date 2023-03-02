fbadfc716d378b7bf47d074204d36ac1439c209e
davidudo <udodavid46.ud@gmail.com>
2023-03-02 09:50:48 +0100

    Did some tasks

--- tests/10-main.c
+++ tests/10-main.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "binary_trees.h"
+#include "../binary_trees.h"
 
 /**
  * main - Entry point
@@ -10,21 +10,20 @@
 int main(void)
 {
     binary_tree_t *root;
-    int full;
+    size_t depth;
 
     root = binary_tree_node(NULL, 98);
     root->left = binary_tree_node(root, 12);
     root->right = binary_tree_node(root, 402);
     binary_tree_insert_right(root->left, 54);
     binary_tree_insert_right(root, 128);
-    root->left->left = binary_tree_node(root->left, 10);
     binary_tree_print(root);
 
-    full = binary_tree_is_full(root);
-    printf("Is %d full: %d\n", root->n, full);
-    full = binary_tree_is_full(root->left);
-    printf("Is %d full: %d\n", root->left->n, full);
-    full = binary_tree_is_full(root->right);
-    printf("Is %d full: %d\n", root->right->n, full);
+    depth = binary_tree_depth(root);
+    printf("Depth of %d: %lu\n", root->n, depth);
+    depth = binary_tree_depth(root->right);
+    printf("Depth of %d: %lu\n", root->right->n, depth);
+    depth = binary_tree_depth(root->left->right);
+    printf("Depth of %d: %lu\n", root->left->right->n, depth);
     return (0);
 }
