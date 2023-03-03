210f51740d8a0ce80cd1711e847083733f469a63
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-02 19:36:51 -0800

    binary trees

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,131 +1,56 @@
 #include "binary_trees.h"
 
-levelorder_queue_t *create_node(binary_tree_t *node);
-void free_queue(levelorder_queue_t *head);
-void push(binary_tree_t *node, levelorder_queue_t *head,
-		levelorder_queue_t **tail);
-void pop(levelorder_queue_t **head);
-int binary_tree_is_complete(const binary_tree_t *tree);
-
 /**
- * create_node - Creates a new levelorder_queue_t node.
- * @node: The binary tree node for the new node to contain.
- *
- * Return: If an error occurs, NULL.
- *         Otherwise, a pointer to the new node.
+ * count_node - counts the number of nodes in a tree
+ * @tree: is a pointer to the root node of the tree to count
+ * Return: size of tree
  */
-levelorder_queue_t *create_node(binary_tree_t *node)
+size_t count_node(const binary_tree_t *tree)
 {
-	levelorder_queue_t *new;
+	size_t size = 0;
 
-	new = malloc(sizeof(levelorder_queue_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->node = node;
-	new->next = NULL;
-
-	return (new);
-}
-
-/**
- * free_queue - Frees a levelorder_queue_t queue.
- * @head: A pointer to the head of the queue.
- */
-void free_queue(levelorder_queue_t *head)
-{
-	levelorder_queue_t *tmp;
-
-	while (head != NULL)
+	if (tree)
 	{
-		tmp = head->next;
-		free(head);
-		head = tmp;
+		size += 1;
+		size += count_node(tree->left);
+		size += count_node(tree->right);
 	}
+	return (size);
 }
 
 /**
- * push - Pushes a node to the back of a levelorder_queue_t queue.
- * @node: The binary tree node to print and push.
- * @head: A double pointer to the head of the queue.
- * @tail: A double pointer to the tail of the queue.
- *
- * Description: Upon malloc failure, exits with a status code of 1.
+ * check_Complete - a function that checks if a binary tree is complete
+ * @tree: is a pointer to the root node of the tree to check
+ * @index: index
+ * @node_count: number of nodes in tree
+ * Return: trueor false
  */
-void push(binary_tree_t *node, levelorder_queue_t *head,
-		levelorder_queue_t **tail)
+bool check_Complete(const binary_tree_t *tree, int index, int node_count)
 {
-	levelorder_queue_t *new;
+	if (tree == NULL)
+		return (true);
 
-	new = create_node(node);
-	if (new == NULL)
-	{
-		free_queue(head);
-		exit(1);
-	}
-	(*tail)->next = new;
-	*tail = new;
+	if (index >= node_count)
+		return (false);
+
+	return (check_Complete(tree->left, 2 * index + 1, node_count)
+			&& check_Complete(tree->right, 2 * index + 2, node_count));
 }
 
 /**
- * pop - Pops the head of a levelorder_queue_t queue.
- * @head: A double pointer to the head of the queue.
- */
-void pop(levelorder_queue_t **head)
-{
-	levelorder_queue_t *tmp;
-
-	tmp = (*head)->next;
-	free(*head);
-	*head = tmp;
-}
-
-/**
- * binary_tree_is_complete - Checks if a binary tree is complete.
- * @tree: A pointer to the root node of the tree to traverse.
- *
- * Return: If the tree is NULL or not complete, 0.
- *         Otherwise, 1.
- *
- * Description: Upon malloc failure, exits with a status code of 1.
+ * binary_tree_is_complete - a function that checks if a binary tree iscomplete
+ * @tree:  is a pointer to the root node of the tree to check
+ * Return: 1 (yes) or 0(no)
+ * If tree is NULL, your function must return 0
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	levelorder_queue_t *head, *tail;
-	unsigned char flag = 0;
+	int node_count = count_node(tree);
 
 	if (tree == NULL)
 		return (0);
 
-	head = tail = create_node((binary_tree_t *)tree);
-	if (head == NULL)
-		exit(1);
-
-	while (head != NULL)
-	{
-		if (head->node->left != NULL)
-		{
-			if (flag == 1)
-			{
-				free_queue(head);
-				return (0);
-			}
-			push(head->node->left, head, &tail);
-		}
-		else
-			flag = 1;
-		if (head->node->right != NULL)
-		{
-			if (flag == 1)
-			{
-				free_queue(head);
-				return (0);
-			}
-			push(head->node->right, head, &tail);
-		}
-		else
-			flag = 1;
-		pop(&head);
-	}
-	return (1);
+	if (check_Complete(tree, 0, node_count))
+		return (1);
+	return (0);
 }
